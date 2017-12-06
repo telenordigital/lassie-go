1a7c095b74cb53ae2d4fd06774871ac7cd1a60cf
Ståle Dahl <stalehd@gmail.com>
2017-12-06 15:23:26 +0100

    Add support for downstream messages (#6)
    ScheduleMessage (aka POST) and GetScheduledMessage (aka GET)

--- device.go
+++ device.go
@@ -17,6 +17,13 @@ type Device struct {
 	Tags                  map[string]string `json:"tags"`
 }
 
+// DownstreamMessage represents downstream messages to devices.
+type DownstreamMessage struct {
+	Port       uint8  `json:"port"`
+	Ack        bool   `json:"ack"`
+	HexPayload string `json:"data"`
+}
+
 // CreateDevice creates a device.
 func (c *Client) CreateDevice(appeui string, dev Device) (Device, error) {
 	err := c.create(fmt.Sprintf("/applications/%s/devices", appeui), &dev)
@@ -48,4 +55,18 @@ func (c *Client) Device(appeui, deveui string) (Device, error) {
 // DeleteDevice deletes a device.
 func (c *Client) DeleteDevice(appeui, deveui string) error {
 	return c.delete(fmt.Sprintf("/applications/%s/devices/%s", appeui, deveui))
+}
+
+// ScheduleMessage schedules a downstream message to a device. Port is in range 1-223.
+// If the ack flag is set the message will be re-sent until the device acknowledges it.
+// The payload is hex encoded.
+func (c *Client) ScheduleMessage(appeui, deveui string, msg DownstreamMessage) error {
+	return c.create(fmt.Sprintf("/applications/%s/devices/%s/message", appeui, deveui), &msg)
+}
+
+// GetScheduledMessage returns the scheduled message for the device.
+func (c *Client) GetScheduledMessage(appeui, deveui string) (DownstreamMessage, error) {
+	var msg DownstreamMessage
+	err := c.get(fmt.Sprintf("/applications/%s/devices/%s/message", appeui, deveui), &msg)
+	return msg, err
 }
